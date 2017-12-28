7cdca66a8cccbfa86110d9e9acb18f18c8bd8cf1
Cristian BSan <cbsan.dev@gmail.com>
2017-12-28 08:10:22 -0200

    Revert "foo"
    This reverts commit 024adf7ee082ec72dbb17290f3665d19918ffc8d.

--- main.cpp
+++ main.cpp
@@ -16,123 +16,16 @@
 #include <dlfcn.h>
 #include "sgfplib.h"
 
-
 using namespace std;
 
 LPSGFPM  sgfplib = NULL;
 
-void procargs(int, char **, float *, char **, char **, int *, int *, int *, int *, int *, char **);
-void print_usage(char *);
-int fail(DWORD err, const char *message);
-int capture();
-int foo(int argc, char **argv);
-void cwsq();
-uint8_t *stringToByteArray(char* arr);
-
-int main(int argc, char **argv) {
-    DWORD err;
-    err = CreateSGFPMObject(&sgfplib);
-    // vector<char*> arg_v;
-    
-    // execl("/usr/local/src/nbis/bin/dwsq", "raw", "./out/dig.wsq");
-    
-    return 0;
-}
-
-/*****************************************************************/
 int fail(DWORD err, const char *message) {
   cerr << "ERROR"
        << " (" << err << "): " << message << endl;
   return err;
 }
 
-/*****************************************************************/
-void cwsq()
-{
-
-}
-
-/*****************************************************************/
-void print_usage(char *arg0)
-{
-   fprintf(stderr, "Usage: %s ", arg0);
-   fprintf(stderr, "<r bitrate> <outext> <image file>\n");
-   fprintf(stderr,
-           "                 [-raw_in w,h,d,[ppi]] [comment file]\n\n");
-   fprintf(stderr,
-           "   r bitrate = compression bit rate (2.25==>5:1, .75==>15:1)\n\n");
-}
-
-/*****************************************************************/
-int foo(int argc, char **argv)
-{
-    DWORD err;
-    SGDeviceInfoParam deviceInfo;
-    BYTE *imageBuffer1;
-    BYTE *imageBuffer2;
-    BYTE *minutiaeBuffer2;
-    DWORD nfiq;
-    DWORD quality;
-    char kbBuffer[100];
-    FILE *fp = NULL;
-    SGFingerInfo fingerInfo;
-    DWORD templateSize, templateSizeMax;
-
-    if (argc < 2) {
-        return fail(1, "Inufficient paramenters");
-    }
-
-    ///////////////////////////////////////////////
-    // Instantiate SGFPLib object
-
-    err = CreateSGFPMObject(&sgfplib);
-    if (!sgfplib)
-    {
-        printf("ERROR - Unable to instantiate FPM object.\n\n");
-        return 0;
-    }
-
-    //err = capture();
-
-    vector<string> fingers;
-    char *fingers_filename = argv[1];
-    ifstream fingers_file (argv[1]);
-    if (fingers_file.is_open()) {
-        string line;
-        while(getline(fingers_file, line)) {
-            fingers.push_back(line);
-        }
-        fingers_file.close();
-    } else {
-        return fail(1, "Invalid fingers file");
-    }
-    
-    char* buffer;
-    int sizeWSQ;
-
-    //while (fingers.size() > 0) {
-        buffer = (char*) fingers.back().c_str();
-        
-        imageBuffer2 = stringToByteArray(buffer);
-        
-        
-        fp = fopen("./out/dig.wsq","wb");
-        fwrite (imageBuffer2 , sizeof (BYTE) , 300*400 , fp);
-        fclose(fp);
-
-        // minutiaeBuffer2 = (BYTE*) malloc(300*400);
-        // fingerInfo.FingerNumber = SG_FINGPOS_UK;
-        // fingerInfo.ViewNumber = 1;
-        // fingerInfo.ImpressionType = SG_IMPTYPE_LP;
-        // fingerInfo.ImageQuality = quality; //0 to 100
-        
-        // //err = sgfplib->SetTemplateFormat(TEMPLATE_FORMAT_ANSI378);
-        // err = sgfplib->CreateTemplate(&fingerInfo, imageBuffer2, minutiaeBuffer2);
-        
-   // }
-}
-
-/*****************************************************************/
 int capture()
 {
     DWORD err;
@@ -155,6 +48,7 @@ int capture()
         return 0;
     }
 
+    
 
     err = sgfplib->OpenDevice(0);
     if (err == SGFDX_ERROR_NONE)
@@ -211,7 +105,6 @@ int capture()
     return err;
 }
 
-/*****************************************************************/
 uint8_t *stringToByteArray(char* arr) {
     string ss(arr);
     int byteArrSize = ss.size() / 2;
@@ -223,5 +116,80 @@ uint8_t *stringToByteArray(char* arr) {
     }
 
     return byteArr;
+}
+
+
+int main(int argc, char **argv) {
+    DWORD err;
+    SGDeviceInfoParam deviceInfo;
+    BYTE *imageBuffer1;
+    BYTE *imageBuffer2;
+    BYTE *minutiaeBuffer2;
+    DWORD nfiq;
+    DWORD quality;
+    char kbBuffer[100];
+    FILE *fp = NULL;
+    SGFingerInfo fingerInfo;
+    DWORD templateSize, templateSizeMax;
+
+    if (argc < 2) {
+        return fail(1, "Inufficient paramenters");
+    }
+
+    ///////////////////////////////////////////////
+    // Instantiate SGFPLib object
+
+    err = CreateSGFPMObject(&sgfplib);
+    if (!sgfplib)
+    {
+        printf("ERROR - Unable to instantiate FPM object.\n\n");
+        return 0;
+    }
+
+    //err = capture();
+
+    vector<string> fingers;
+    char *fingers_filename = argv[1];
+    ifstream fingers_file (argv[1]);
+    if (fingers_file.is_open()) {
+        string line;
+        while(getline(fingers_file, line)) {
+            fingers.push_back(line);
+        }
+        fingers_file.close();
+    } else {
+        return fail(1, "Invalid fingers file");
+    }
+    
+    char* buffer;
+    int sizeWSQ;
+
+    
+    
+    
+   
+    
+    //while (fingers.size() > 0) {
+        buffer = (char*) fingers.back().c_str();
+        
+        imageBuffer2 = stringToByteArray(buffer);
+        
+        
+        fp = fopen("./out/dig.wsq","wb");
+        fwrite (imageBuffer2 , sizeof (BYTE) , 300*400 , fp);
+        fclose(fp);
+
+        // minutiaeBuffer2 = (BYTE*) malloc(300*400);
+        // fingerInfo.FingerNumber = SG_FINGPOS_UK;
+        // fingerInfo.ViewNumber = 1;
+        // fingerInfo.ImpressionType = SG_IMPTYPE_LP;
+        // fingerInfo.ImageQuality = quality; //0 to 100
+        
+        // //err = sgfplib->SetTemplateFormat(TEMPLATE_FORMAT_ANSI378);
+        // err = sgfplib->CreateTemplate(&fingerInfo, imageBuffer2, minutiaeBuffer2);
+        
+   // }
+    
+    return 0;
 }
 
